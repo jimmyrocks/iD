861f047eb93229a5fe0d10dc76edc5ddab568de0
Jim McAndrew <jim@loc8.us>
2015-05-02 09:31:51 -0600

    dev and test commits: Update js/id/renderer/conflation_layer.js

--- js/id/renderer/conflation_layer.js
+++ js/id/renderer/conflation_layer.js
@@ -1,83 +1,24 @@
 iD.ConflationLayer = function() {
-    var projection,
-        gj = {},
-        enable = true,
-        svg;
+  var enable =  true;
 
-    function render(selection) {
-        svg = selection.selectAll('svg')
-            .data([render]);
+  function render(_) {
+    // render.enable(true);
+  }
 
-        svg.enter()
-            .append('svg');
-
-        svg.style('display', enable ? 'block' : 'none');
-
-        var paths = svg
-            .selectAll('path')
-            .data([gj]);
-
-        paths
-            .enter()
-            .append('path')
-            .attr('class', 'conflation');
-
-        var path = d3.geo.path()
-            .projection(projection);
-
-        paths
-            .attr('d', path);
-
-        if (typeof gj.features !== 'undefined') {
-            svg
-                .selectAll('text')
-                .remove();
-
-            svg
-                .selectAll('path')
-                .data(gj.features)
-                .enter()
-                .append('text')
-                .attr('class', 'conflation')
-                .text(function(d) {
-                    return d.properties.desc || d.properties.name;
-                })
-                .attr('x', function(d) {
-                    var centroid = path.centroid(d);
-                    return centroid[0] + 5;
-                })
-                .attr('y', function(d) {
-                    var centroid = path.centroid(d);
-                    return centroid[1];
-                });
-        }
-    }
-
-    render.projection = function(_) {
-        if (!arguments.length) return projection;
-        projection = _;
-        return render;
-    };
-
-    render.enable = function(_) {
-        if (!arguments.length) return enable;
-        enable = _;
-        return render;
-    };
-
-    render.geojson = function(_) {
-        if (!arguments.length) return gj;
-        gj = _;
-        return render;
-    };
-
-    render.dimensions = function(_) {
-        if (!arguments.length) return svg.dimensions();
-        svg.dimensions(_);
-        return render;
-    };
-
-    render.id = 'layer-conflation';
-
+  render.enable = function(_) {
+    console.log('enable?', _);
+    if (!arguments.length) return enable;
+    enable = _;
+    console.log('returning', _);
     return render;
+  };
+
+  render.disable = function(_) {
+    if (!arguments.length) return enable;
+    return render.enable(!_);
+  };
+
+  render.id = 'layer-conflation';
+
+  return render;
 };
