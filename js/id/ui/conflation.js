3abd7a245bccff6b77ed0ef242fed92274a804ec
Jim McAndrew <jim@loc8.us>
2015-05-21 15:18:17 -0600

    Add GeoJSON layer

--- js/id/ui/conflation.js
+++ js/id/ui/conflation.js
@@ -1,6 +1,5 @@
 iD.ui.Conflation = function(context) {
-  var dispatch = d3.dispatch('falsePositive', 'skip', 'save', 'cancel'),
-    list;
+  var dispatch = d3.dispatch('falsePositive', 'skip', 'save', 'cancel');
 
   var apiServer = 'localhost:25000';
 
@@ -34,27 +33,36 @@ iD.ui.Conflation = function(context) {
     var welcomeScreen = body.append('div');
 
     welcomeScreen.append('h2').text('Welcome to MapRoulette Conflation!');
-    var pickAChallenge = body.append('div').text('Pick a challenge');
+    var pickAChallenge = body.append('div').text('Pick a challenge: ');
+    var taskInstruction = body.append('div');
 
     var options, change = function() {
       var selectedIndex = challengeDropDown.property('selectedIndex'),
         data = options[0][selectedIndex].__data__;
-        var taskUrl = 'http://' + apiServer + '/api/challenge/' + data.slug + '/task';
-        d3.json(taskUrl, function(e,r) {
-          welcomeScreen.style('display', 'none');
-          d3.json(taskUrl + '/' + r.identifier + '/geometries', function(ge, gr) {
-            console.log(gr);
-            console.log(gr.features[0].properties.osmid);
-            context.zoomToEntity('w' + gr.features[0].properties.osmid, true);
-          });
-          console.log(r);
+      var taskUrl = 'http://' + apiServer + '/api/challenge/' + data.slug + '/task';
+      d3.json(taskUrl, function(e, r) {
+        taskInstruction.text(r.instruction);
+        welcomeScreen.style('display', 'none');
+        d3.json(taskUrl + '/' + r.identifier + '/geometries', function(ge, gr) {
+          var osmid;
+          if (gr.features[0].properties.osmid) {
+            osmid = gr.features[0].properties.osmid;
+            gr.features.splice(0, 1);
+          }
+          // draw all other features to draw
+          context.background().setConflationGeoJSON(gr);
+          if (osmid) {
+            context.zoomToEntity('w' + osmid, true);
+          } else {
+            context.map().centerZoom(d3.geo.centroid(gr), context.map().zoom());
+          }
         });
+      });
 
     };
     var challengeDropDown = pickAChallenge.append('select').on('change', change);
 
     d3.json('http://' + apiServer + '/api/challenges', function(e, r) {
-      console.log(e, r);
       if (!e) {
         options = challengeDropDown.selectAll('option')
           .data(r);
